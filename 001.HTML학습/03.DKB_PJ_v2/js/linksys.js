8ea74d8743d5497a8c680c75f2292389f8c84ae1
dlathwjd19 <nancy154@naver.com>
2022-03-29 15:15:05 +0900

    29

---
+++ 001.HTML학습/03.DKB_PJ_v2/js/linksys.js
@@ -0,0 +1,11 @@
+// 도깨비 PJ 링크 시스템 JS - linksys.js
+
+//////////// 로드구역 //////////////////////////////////////
+// DOMContentLoaded - html코드가 모두 로딩되면 실행
+window.addEventListener("DOMContentLoaded", ()=>{
+
+    console.log("로딩완료!");
+
+    
+
+}); //////////////// 로드구역 //////////////////////////////////
