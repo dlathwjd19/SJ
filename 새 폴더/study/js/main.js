d6d0b6f80d414d155dacbe47ff939f845679f4de
dlathwjd19 <nancy154@naver.com>
2022-04-07 11:07:50 +0900

    Update main.js

--- 새 폴더/study/js/main.js
+++ 새 폴더/study/js/main.js
@@ -21,7 +21,7 @@ window.addEventListener("load",() => {
         "위 베이비 베어스":"xkBPklt3Ju4",
         "슈퍼텐 시간탐험대":"1gyvD5fxNIE",
         "그리지와 레밍스 월드투어":"01wgWeMonWQ",
-        "브레드 이발소 시즌2":"k9A7TxQWnjU",
+        "브레드 이발소 시즌2":"k9A7TxQWnjU"
     }; ///// mcode객체 /////
 
     // 포스터 a요소 클릭시 프로그램 변경하기
@@ -39,28 +39,4 @@ window.addEventListener("load",() => {
             
         }; ///// click 함수 /////
     } /////////// for of ////////////
-
-    // 포스터 메뉴 li 클릭시 li에 클래스 on넣기
-    // 대상: .glist ul>li
-    // let plist = document.querySelectorAll(".plist ul>li");
-
-    // console.log("리스트개수:",plist.length);
-
-    // 클래스 초기화 함수 ////////
-    // function resetFn() {
-    //     for (let x of plist)
-    //         x.classList.remove("on");
-    // }
-
-    // 대상만큼 click이벤트 설정하기
-    // for of 사용!
-    // for(let x of plist){
-    //     x.onclick = ()=>{
-    //         // 1. 클래스 초기화함수 호출
-    //         resetFn();
-    //         // 2. 해당 li요소 클래스 on 넣기
-    //         x.classList.add("on");
-    //     }; 
-    // }
-
 }); 
