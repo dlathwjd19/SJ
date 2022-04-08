657367a0f6c2f8b7234e1d3240462fb80d9ff684
dlathwjd19 <nancy154@naver.com>
2022-04-08 10:44:31 +0900

    8

--- 001.HTML학습/08.VOGUE_PJ/002.site/js/common.js
+++ 001.HTML학습/08.VOGUE_PJ/002.site/js/common.js
@@ -5,6 +5,8 @@ window.addEventListener("DOMContentLoaded", () => {
 
     console.log("로딩완료!");
 
+    // 부드러운 스크롤
+
     // 스크롤값 변수
     let scTop;
     // 상단영역 - #top
@@ -56,25 +58,22 @@ window.addEventListener("DOMContentLoaded", () => {
         
        // 현재 스크롤 위치가 등장할 요소의 위치범위에
        // 있다면 등장해라!
-       if(scTop > scPos[0]-winH && scTop < scPos[0]){
-            scAct[0].classList.add("on");
-       } 
-
-
-
-
-
-
-
+     
+        // 등장요소 위치값 배열만큼 체크함수 호출!
+        scPos.forEach((val,idx)=>scAction(idx));
 
 
     }); //////////////// scroll /////////////////
 
     /*********************************************** 
-        원리:
-            scAct 변수에 .scAct 클래스 요소를 담고
-            요소의 개수만큼 scPos 배열변수에 위치값을
-            셋팅한다!
+        ** 원리:
+        1. scAct 변수에 .scAct 클래스 요소를 담고
+        요소의 개수만큼 scPos 배열변수에 위치값을
+        셋팅한다!
+
+        2. 등장요소의 위치보다 이전부터 위치값 사이 범위에
+        스크롤이 통과할때 등장할 요소를 나타나도록 
+        함수로 체크한다!
         
     ***********************************************/
     // 스크롤 등장 대상요소
@@ -90,7 +89,25 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // 스크롤 등장위치 조정값 : 윈도우 화면크기의 2 / 3
     const winH = (window.innerHeight / 3) * 2 ;
-    console.log("윈도우높이절반",winH);
+    console.log("윈도우높이2/3",winH);
+
+    /************************************************** 
+        함수명: scAction
+        기능: 스크롤 위치 값이 설정범위에 들어가면
+            해당 순번의 요소가 등장한다!
+    **************************************************/
+   const scAction = seq => { // seq는 순번
+        // console.log("순번",seq);
+
+        // 조건: 스크롤위치가 포지션위치이전 보다 크고 
+        // 스크롤 위치가 포지션위치보다 작으면 실행!
+        if(scTop > scPos[seq]-winH && 
+            scTop < scPos[seq]){
+            // 해당 순번요소에 클래스 on 넣기!
+            scAct[seq].classList.add("on");
+        } /////// if //////////
+
+   }; //////// scAction 함수 ////////////////////
 
 
 
