22e916d1577b077221a98fe86d9e461c106f5863
dlathwjd19 <nancy154@naver.com>
2022-04-14 17:19:16 +0900

    14

--- 001.HTML학습/08.VOGUE_PJ/002.site/js/gallery.js
+++ 001.HTML학습/08.VOGUE_PJ/002.site/js/gallery.js
@@ -1,1 +1,23 @@
 // 회원가입 갤러리 JS - gallery.js
+
+$(()=>{ //////////////////////// jQB //////////////////////
+
+    console.log("로딩완료!");
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+
+}); //////////////////////// jQB //////////////////////////
