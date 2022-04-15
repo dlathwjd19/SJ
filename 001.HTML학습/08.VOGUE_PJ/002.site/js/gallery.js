03d4a9b5324838d4aaa39b2bc2128abd1880fa63
dlathwjd19 <nancy154@naver.com>
2022-04-15 10:15:21 +0900

    15

--- 001.HTML학습/08.VOGUE_PJ/002.site/js/gallery.js
+++ 001.HTML학습/08.VOGUE_PJ/002.site/js/gallery.js
@@ -5,10 +5,10 @@ $(()=>{ //////////////////////// jQB //////////////////////
     console.log("로딩완료!");
 
     var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        slidesPerGroup: 3,
-        loop: true,
+        slidesPerView: 3, // 한 화면당 슬라이드 개수
+        spaceBetween: 30, // 슬라이드 사이간격(px)
+        slidesPerGroup: 3, // 슬라이드 묶음 개수(넘기는단위)
+        loop: true, // 무한루프(기본값flase)
         loopFillGroupWithBlank: true,
         pagination: {
           el: ".swiper-pagination",
