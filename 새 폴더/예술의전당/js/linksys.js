6a446f4fc98b7664a7047b39923b5c9d3ecba7c9
dlathwjd19 <nancy154@naver.com>
2022-04-18 03:17:25 +0900

    18

--- 새 폴더/예술의전당/js/linksys.js
+++ 새 폴더/예술의전당/js/linksys.js
@@ -4,5 +4,16 @@
 $(()=>{ ////// jQB //////
 
     console.log("로딩완료!");
+
+    var swiper = new Swiper(".mySwiper", {
+        direction: "vertical",
+        slidesPerView: 1,
+        spaceBetween: 30,
+        mousewheel: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
     
 }) ////// jQB //////
