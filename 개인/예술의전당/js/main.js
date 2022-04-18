cbf85d95bd15fac44be3bb406ecf3da019c9ef9f
dlathwjd19 <nancy154@naver.com>
2022-04-19 01:10:38 +0900

    00

--- 개인/예술의전당/js/main.js
+++ 개인/예술의전당/js/main.js
@@ -1,15 +1,27 @@
-// 예술의 전당 페이지 JS -linksys.js
+// 예술의 전당 페이지 JS -main.js
 
 
 $(()=>{ ////// jQB //////
+	
+	console.log("로딩완료!!");
 
-    console.log("로딩완료!");
+	var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 4, // 한 화면당 슬라이드 개수
+    	spaceBetween: 30, // 슬라이드 사이간격(px)
+		
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
 
-    $('#fullpage').fullpage({
-		//options here
-		autoScrolling:true,
-		scrollHorizontally: true
-	});
 
     
 }); ////// jQB //////
